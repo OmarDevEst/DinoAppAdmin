940920f602159043e0a0dc21cdc340961803411e
Edwardreyessp <edwardreyessp@gmail.com>
2023-09-18 23:48:33 -0600

    feat(firebase): Intregración de Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,13 @@ import 'config/app_theme.dart';
 
 import 'Pages/Home/Home.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
