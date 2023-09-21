5df09d0730ff594bbd63cac0a4ac3aef9cadfeef
Edwardreyessp <edwardreyessp@gmail.com>
2023-09-20 22:53:58 -0600

    feat(firebase): Finalización de los endpoint
    Se identificaron dos endpoint, uno para el administrador y otro para el profesor, en el caso del administrador podría haber una posibilidad de crear un endpoint para los distintos grupos en caso de ser muchos para no saturar un endpoint con tanta información

--- lib/Pages/Home/Home.dart
+++ lib/Pages/Home/Home.dart
@@ -18,7 +18,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: getInstitute(),
+      future: getTeacherGroups(),
       builder: (((context, snapshot) {
         return const HomePage();
       })),
--- lib/firebase/service.dart
+++ lib/firebase/service.dart
@@ -12,8 +12,27 @@ Future<Map<String, dynamic>> getInstitute() async {
         .doc('KAbIrT1ER6VW2zR1MT89')
         .get()
         .then((value) {
-      log(value.data()!['groups'].toString());
+      log(value.data().toString());
       instituteList = value.data()!;
+    });
+    return instituteList;
+  } catch (e) {
+    return {};
+  }
+}
+
+Future<Map<String, dynamic>> getTeacherGroups() async {
+  Map<String, dynamic> instituteList = {};
+  try {
+    await firestore
+        .collection('instituciones')
+        .doc('KAbIrT1ER6VW2zR1MT89')
+        .collection('teachers')
+        .doc('Z5xeFfRHwIqSXFU0xLvy')
+        .get()
+        .then((value) {
+      log(value.data()!['groups'].toString());
+      instituteList = value.data()!['groups'];
     });
     return instituteList;
   } catch (e) {
